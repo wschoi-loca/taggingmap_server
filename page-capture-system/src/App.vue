196043e48499268d5e884a325471f7fea38950d3
wschoi-loca <choiwonseok@lottecard.co.kr>
2025-03-13 16:57:25 +0900

    a

---
+++ page-capture-system/src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <h1>Page Capture System</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Image</th>
+          <th>Title</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="page in pages" :key="page._id">
+          <td>
+            <img :src="page.image" alt="Captured Image" width="100" />
+          </td>
+          <td>{{ page.title }}</td>
+          <td><a :href="page.url" target="_blank">{{ page.url }}</a></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      pages: []
+    };
+  },
+  created() {
+    this.fetchPages();
+  },
+  methods: {
+    async fetchPages() {
+      try {
+        const response = await axios.get('http://localhost:5000/api/pages');
+        this.pages = response.data;
+      } catch (error) {
+        console.error('Error fetching pages:', error);
+      }
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+table {
+  margin: 0 auto;
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+
+img {
+  max-width: 100px;
+}
+</style>
