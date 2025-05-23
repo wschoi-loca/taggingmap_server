977d612ef3efee547decfad2a862a7a1c1a9b8af
wschoi-loca <choiwonseok@lottecard.co.kr>
2025-05-23 14:20:11 +0900

    Update CSP headers to allow Google sign-in styles

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
     'Content-Security-Policy',
     "default-src 'self'; " +
     "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://*.googleapis.com https://*.gstatic.com; " +
-    "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com; " +
-    "img-src 'self' https://*.googleapis.com https://*.gstatic.com https://res.cloudinary.com data:; " +
+    "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com https://accounts.google.com; " +
+    "img-src 'self' https://*.googleapis.com https://*.gstatic.com https://res.cloudinary.com https://accounts.google.com data:; " +
     "connect-src 'self' https://*.googleapis.com https://accounts.google.com https://apis.google.com; " +
     "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com data:; " +
     "frame-src https://accounts.google.com https://content.googleapis.com; " +
